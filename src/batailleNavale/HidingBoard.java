f6df7d9000fa3cc8a29ae5049464ba220d1e0172
svetlana <s.sadkovskaia@campus-igs-toulouse.fr>
2019-12-02 14:55:24 +0100

    initial commit

---
+++ src/batailleNavale/HidingBoard.java
@@ -0,0 +1,16 @@
+package batailleNavale;
+import java.util.HashMap;
+
+public class HidingBoard {
+	
+	HashMap<String, Integer> coordinates = new HashMap<String, Integer>();
+	
+	
+	public Board(coordinates){
+		
+	}
+	
+	public void squareStatus( coordinates) {
+		
+	}
+}
---
+++ src/batailleNavale/Ship.java
@@ -0,0 +1,24 @@
+package batailleNavale;
+
+public class Ship {
+	int model;
+	String orientation;
+	int coordinates;
+	
+	public Ship(int model,String orientation,int coordinates){
+
+	}
+	
+	public boolean isOnSquare(int coordinates) {
+		
+	}
+	
+	public void isSunk() {
+		
+	}
+
+}	
+	
+	
+	
+
